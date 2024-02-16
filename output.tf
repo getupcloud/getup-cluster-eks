310a12602802467734e85f3f21d511a54c471c67
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-02-16 21:13:27 +0000

    Better monitoring
    Adds prom-rules
    Adds teleport
    Create customer configs

--- output.tf
+++ output.tf
@@ -1,6 +1,21 @@
+output "customer_name" {
+  description = "Name of the customer."
+  value       = var.customer_name
+}
+
 output "cluster_name" {
   description = "EKS cluster name."
   value       = module.eks.cluster_name
+}
+
+output "cluster_provider" {
+  description = "Cloud Provider or platform where cluster is running."
+  value       = var.cluster_provider
+}
+
+output "cluster_sla" {
+  description = "SLA fot the cluster."
+  value       = var.cluster_sla
 }
 
 output "aws_load_balancer_controller_iam_role_arn" {
@@ -20,5 +35,5 @@ output "loki_s3_bucket_name" {
 
 output "cert_manager_iam_role_arn" {
   description = "Cert-Manager Role ARN."
-  value       = module.cert_manager_irsa.iam_role_arn
+  value       = module.cert-manager.cert_manager_iam_role_arn
 }
---
+++ variables-customer.tf
@@ -0,0 +1,24 @@
+variable "customer_name" {
+  description = "Name of the customer."
+  type        = string
+}
+
+variable "cluster_provider" {
+  description = "Cloud Provider or platform where cluster is running."
+  type        = string
+
+  validation {
+    condition     = contains(["eks", "aks", "doks", "gke", "oke", "on-prem", "okd", "okd3", "rancher", "none"], var.cluster_provider)
+    error_message = "The Cluster Provider is invalid."
+  }
+}
+
+variable "cluster_sla" {
+  description = "SLA fot the cluster."
+  type        = string
+
+  validation {
+    condition     = contains(["high", "low", "none"], var.cluster_sla)
+    error_message = "The Cluster SLA is invalid."
+  }
+}
