0a14f1ba4b9b5f3c643ba8095f65a9afa78645fd
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2025-03-06 00:21:08 +0000

    Auto-generated commit

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region = var.aws_region
-  allowed_account_ids = [ var.aws_account_id ]
+  region              = var.aws_region
+  allowed_account_ids = [var.aws_account_id]
 
   default_tags {
     tags = var.tags
