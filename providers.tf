53a66690e65fd2c428d20b95cc3b4512630aeac1
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2025-03-24 16:35:53 +0000

    Add auth vars

--- providers.tf
+++ providers.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region              = var.aws_region
   allowed_account_ids = [var.aws_account_id]
+  access_key          = var.aws_access_key_id
+  secret_key          = var.aws_secret_access_key
 
   default_tags {
     tags = var.tags
---
+++ variables-auth.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID."
+  type        = string
+  default     = null
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key."
+  type        = string
+  default     = null
+}
